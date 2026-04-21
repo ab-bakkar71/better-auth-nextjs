9d2bf23cd8322e230fbd0f80e47010a79fa77a4b
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-04-22 00:05:20 +0600

    mongoDB & better auth setup and hero ui, gravity icon  install done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,12 +2,30 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
+
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
 });
+
+
+
+
+
+/**
+ * 1. better auth: install
+ * 2. .env variable create
+ *    -BETTER_AUTH_SECRET=bV2PhB0QymeQaneTp2hsUm559RHaqZky
+      -BETTER_AUTH_URL=http://localhost:3000
+ * 3. create auth instance
+ * 4. use auth instance in your app
+ * 5. 
+ * / 
